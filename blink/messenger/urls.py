90b9c718ec4372d493651515b4406244c040b1e7
Твоё Имя <_@example.com>
2025-04-01 21:42:04 +0300

    routing for furas pages

---
+++ blink/messenger/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+# импортируем вьюху 
+from . import views
+
+
+urlpatterns = [
+    path('login/', views.login_page),
+    path('register/', views.register_page),
+    path('login/', views.chat_page),
+
+]
+
+
